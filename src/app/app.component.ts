c9f9172f76071a8f3bb0a87bd508c89bd1e00ff1
Manrique <bl0208032017@unab.edu.sv>
2022-09-30 00:03:10 -0600

    se agrego el proyecto

---
+++ src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'wikiapp';
+  sidenav: any;
+
+  constructor(private observer: BreakpointObserver) {}
+
+  ngAfterViewInit() {
+    this.observer.observe(['(max-width: 800px)']).subscribe((res) => {
+      if (res.matches) {
+        this.sidenav.mode = 'over';
+        this.sidenav.close();
+      } else {
+        this.sidenav.mode = 'side';
+        this.sidenav.open();
+      }
+    });
+  }
+}
+
